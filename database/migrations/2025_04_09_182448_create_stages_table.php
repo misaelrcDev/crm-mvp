a9cca7bc55c73ca0fa29e7888fbcc792749d6147
Misael Cabral <misaelrc.dev@gmail.com>
2025-04-09 15:25:57 -0300

    Criado a model/migration para Estágios

--- database/migrations/2025_04_09_182448_create_stages_table.php
+++ database/migrations/2025_04_09_182448_create_stages_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sales_funnels', function (Blueprint $table) {
+        Schema::create('stages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sales_funnels');
+        Schema::dropIfExists('stages');
     }
 };
